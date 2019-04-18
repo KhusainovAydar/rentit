f2f5341ebf50edc6496dce2a1cc6ba2c629b917b
cracker <cracker@yandex-team.ru>
2019-04-18 23:02:28 +0300

    add travel time fetching

--- main.go
+++ main.go
@@ -3,22 +3,37 @@ package main
 import (
 	"fmt"
 
+	"github.com/the-fusy/rentit/maps"
+
 	"github.com/the-fusy/rentit/flat"
 	"github.com/the-fusy/rentit/parser"
 )
 
 func main() {
+	address := "Москва Льва Толстого 16"
+	latitude, longitude, _ := maps.GetCoordinates(&address)
+	lvaTolstogo := maps.Place{
+		Latitude:  latitude,
+		Longitude: longitude,
+	}
+
 	cianParser := &parser.ParserCian{}
 	flatsRequest := parser.FlatsRequest{
 		City: flat.MOSCOW,
 	}
 	flats := flatsRequest.GetFlats(cianParser, 1)
+
 	for i := range flats {
 		err := flats[i].FillCoordinates()
 		if err != nil {
 			fmt.Printf("ERROR %s\n", err.Error())
 		} else {
-			fmt.Println(flats[i])
+			flat := maps.Place{
+				Latitude:  flats[i].Latitude,
+				Longitude: flats[i].Longitude,
+			}
+			travelTime, _ := maps.GetTravelTime(flat, lvaTolstogo)
+			fmt.Println(flats[i].URL, travelTime/60, "minutes")
 		}
 	}
 }
--- maps/maps.go
+++ maps/maps.go
@@ -3,6 +3,7 @@ package maps
 import (
 	"encoding/json"
 	"errors"
+	"fmt"
 	"io/ioutil"
 	"net/http"
 	"net/url"
@@ -10,13 +11,12 @@ import (
 	"github.com/the-fusy/rentit/config"
 )
 
-func GetCoordinates(address *string) (latitude float64, longitude float64, err error) {
-	hereURL, _ := url.Parse("https://geocoder.api.here.com/6.2/geocode.json")
-	hereParams := url.Values{}
-	hereParams.Set("app_id", config.HereAppID)
-	hereParams.Set("app_code", config.HereAppCode)
-	hereParams.Set("searchtext", *address)
-	hereURL.RawQuery = hereParams.Encode()
+func parseFromHere(URL *string, params *url.Values, result interface{}) (err error) {
+	hereURL, _ := url.Parse(*URL)
+	params.Set("app_id", config.HereAppID)
+	params.Set("app_code", config.HereAppCode)
+	params.Set("jsonAttributes", "0")
+	hereURL.RawQuery = params.Encode()
 
 	res, err := http.Get(hereURL.String())
 	if err != nil {
@@ -29,29 +29,48 @@ func GetCoordinates(address *string) (latitude float64, longitude float64, err e
 		return
 	}
 
-	result := geocode{}
-	err = json.Unmarshal(bodyBytes, &result)
+	err = json.Unmarshal(bodyBytes, result)
 	if err != nil {
 		return
 	}
 
-	err = errors.New("Wrong address")
+	return
+}
+
+func GetCoordinates(address *string) (latitude float64, longitude float64, err error) {
+	hereURL := "https://geocoder.api.here.com/6.2/geocode.json"
+	params := url.Values{
+		"searchtext": []string{*address},
+	}
+	result := geocode{}
+
+	err = parseFromHere(&hereURL, &params, &result)
+	if err != nil {
+		return
+	}
+
+	err = errors.New("Error to get coordinates")
+
 	view := &result.Response.View
 	if len(*view) == 0 {
 		return
 	}
+
 	viewResult := &(*view)[0].Result
 	if len(*viewResult) == 0 {
 		return
 	}
+
 	navigationPosition := &(*viewResult)[0].Location.NavigationPosition
 	if len(*navigationPosition) == 0 {
 		return
 	}
-	err = nil
 
 	latitude = (*navigationPosition)[0].Latitude
 	longitude = (*navigationPosition)[0].Longitude
+	if latitude != 0.0 && longitude != 0.0 {
+		err = nil
+	}
 
 	return
 }
@@ -66,6 +85,51 @@ type geocode struct {
 						Longitude float64
 					}
 				}
+			}
+		}
+	}
+}
+
+func GetTravelTime(from, to Place) (travelTime int16, err error) {
+	hereURL := "https://route.api.here.com/routing/7.2/calculateroute.json"
+	params := url.Values{
+		"mode":           []string{"fastest;publicTransport"},
+		"representation": []string{"overview"},
+		"waypoint0":      []string{fmt.Sprintf("geo!%v,%v", from.Latitude, from.Longitude)},
+		"waypoint1":      []string{fmt.Sprintf("geo!%v,%v", to.Latitude, to.Longitude)},
+	}
+	result := calculateroute{}
+
+	err = parseFromHere(&hereURL, &params, &result)
+	if err != nil {
+		return
+	}
+
+	err = errors.New("Error to get travel time")
+
+	route := result.Response.Route
+	if len(route) == 0 {
+		return
+	}
+
+	travelTime = route[0].Summary.TravelTime
+	if travelTime != 0 {
+		err = nil
+	}
+
+	return
+}
+
+type Place struct {
+	Latitude  float64
+	Longitude float64
+}
+
+type calculateroute struct {
+	Response struct {
+		Route []struct {
+			Summary struct {
+				TravelTime int16
 			}
 		}
 	}

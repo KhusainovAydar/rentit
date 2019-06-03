945570d407ffcaf1b0f9e9ae47991f96c518adaf
DoomzD <doomzdayforwork@gmail.com>
2019-06-03 17:22:58 +0300

    Little improvements.

--- parser/helper.py
+++ parser/helper.py
@@ -52,7 +52,7 @@ class Parser:
             updated_fields['updated_fields'] = document['updated_fields']
             updated_fields['updated_at'] = data['updated_at']
 
-            self.db.collection(MONGO_FLATS).find_one_and_update(
+            self.db.collection(MONGO_FLATS).update_one(
                 {'url': data['url']},
                 {'$set': updated_fields},
                 upsert=False,
